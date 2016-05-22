766e8b8e8506639c119f4d88e571560799cd99d7
Antoine <antoinegreensr@gmail.com>
2016-05-22 21:45:38 +0000

    Create articles table, model and added validations

---
+++ app/models/article.rb
@@ -0,0 +1,5 @@
+class Article < ActiveRecord::Base
+  # Makes sure there must be a title and description before it is saved to the database.
+  validates :title, presence: true length: {minimum: 3, maximum: 50}
+  validates :description, presence: true, length: { minimum 10, maximum: 300}
+end
