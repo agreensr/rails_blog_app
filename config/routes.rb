b488344f033877bbb0ef9ae14549962a789386b3
Antoine <antoinegreensr@gmail.com>
2016-05-22 22:27:43 +0000

    created new template, action and route

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,9 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   root 'pages#home'
   get 'about', to: 'pages#about'
+  
+  # Creates my articles route paths ie index, edit, new, show, update, destroy
+  resources :articles
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
