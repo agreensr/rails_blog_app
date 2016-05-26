e6912a3360e033d11eb118b54978db5a09174a7f
cobra390hp1 <antoinegreensr@gmail.com>
2016-05-25 22:16:43 -0500

    Added pagination to the user and index views

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -5,7 +5,7 @@ class ArticlesController < ApplicationController
 
   def index
     # This displays all of the articles in the database using @articles plural, then calling Article.all
-    @articles = Article.all
+    @articles = Article.paginate(page: params[:page], per_page: 5)
   end
   def new
     @article = Article.new
