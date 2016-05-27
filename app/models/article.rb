5d6a82746c1824136915e5b653bc37f7c590281d
cobra390hp1 <antoinegreensr@gmail.com>
2016-05-27 18:16:33 -0500

    Updated my articles.rb add forgot a curly brace

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,7 +1,7 @@
 class Article < ActiveRecord::Base
   # Makes sure there must be a title and description before it is saved to the database.
   belongs_to :user
-  validates :title, presence: true, length: {minimum: 3, maximum: 100
+  validates :title, presence: true, length: {minimum: 3, maximum: 100}
   validates :description, presence: true, length: { minimum: 10, maximum: 1000}
   validates :user_id, presence: true
 end
