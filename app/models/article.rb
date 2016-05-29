ce1ea6bfc3aa7b0f4b868accd52431607ac8e516
cobra390hp1 <antoinegreensr@gmail.com>
2016-05-29 16:14:16 -0500

    Added article and category associations

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,8 @@
 class Article < ActiveRecord::Base
   # Makes sure there must be a title and description before it is saved to the database.
   belongs_to :user
+  has_many :article_categories
+  has_many :categories, through: :article_categories
   validates :title, presence: true, length: {minimum: 3, maximum: 100}
   validates :description, presence: true, length: { minimum: 10, maximum: 1000}
   validates :user_id, presence: true
